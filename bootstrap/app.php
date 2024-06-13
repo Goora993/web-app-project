d9989291ab1d93656303b1b3cd4bfb28b8c322b1
unknown <hubertxbox@o2.pl>
2024-06-13 23:15:50 +0200

    add endpoint author

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        using: function () {
+            Route::middleware('web')
+                ->group(base_path('routes/authors.php'));
+
+        },
+
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
