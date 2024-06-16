9d3e7e6bf5140c1d3cd560ef7b561876389eea48
Dawid <gorkiewicz.dawid@gmail.com>
2024-06-16 13:25:43 +0200

    WAP-5: app.php routing refactor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         using: function () {
             Route::middleware('web')
                 ->group(base_path('routes/author.php'));
@@ -14,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::middleware('web')
                 ->group(base_path('routes/ebook.php'));
         },
-
+        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
